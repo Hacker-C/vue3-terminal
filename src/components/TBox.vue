2c6cf55feda9236fbb99c3bff0c64db660128dc3
Hacker-C <1507559148@qq.com>
2022-08-09 22:24:40 +0800

    🎉 Init: Initial commit

---
+++ src/components/TBox.vue
@@ -0,0 +1,7 @@
+<script setup lang="ts">
+const msg = ref('hello world')
+</script>
+
+<template>
+  <h1 class="font-bold text-red-500 text-3xl text-center">{{ msg }}</h1>
+</template>
---
+++ vite.config.ts
@@ -0,0 +1,39 @@
+import { fileURLToPath, URL } from 'node:url'
+
+import { defineConfig } from 'vite'
+import vue from '@vitejs/plugin-vue'
+
+import AutoImport from 'unplugin-auto-import/vite'
+import Components from 'unplugin-vue-components/vite'
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  plugins: [
+    vue(),
+    AutoImport({
+      include: [
+        /\.[tj]sx?$/, // .ts, .tsx, .js, .jsx
+        /\.vue$/,
+        /\.vue\?vue/, // .vue
+        /\.md$/ // .md
+      ],
+      imports: ['vue'], // 自动引入外部 api
+      dirs: ['./src/hooks'], // 自动引入 hooks
+      dts: './auto-imports.d.ts',
+      // eslint报错解决
+      eslintrc: {
+        enabled: false, // 默认 false，需要更新配置时开启，要重新运行项目生成
+        filepath: './.eslintrc-auto-import.json'
+      }
+    }),
+    Components({
+      dirs: ['src/components'], // 自动引入 components 中的组件
+      resolvers: []
+    })
+  ],
+  resolve: {
+    alias: {
+      '@': fileURLToPath(new URL('./src', import.meta.url))
+    }
+  }
+})
