62c4abb1df27712204ce509ab8788f4f1c43bd41
Hacker-C <1507559148@qq.com>
2022-08-10 14:05:01 +0800

    🔧 chore: 导入 pinia

--- vite.config.ts
+++ vite.config.ts
@@ -19,12 +19,12 @@ export default defineConfig({
         /\.vue\?vue/, // .vue
         /\.md$/ // .md
       ],
-      imports: ['vue'], // 自动引入外部 api
+      imports: ['vue', 'pinia'], // 自动引入外部 api
       dirs: ['./src/hooks'], // 自动引入 hooks
       dts: './auto-imports.d.ts',
       // eslint报错解决
       eslintrc: {
-        enabled: false, // 默认 false，需要更新配置时开启，要重新运行项目生成
+        enabled: true, // 默认 false，需要更新配置时开启，要重新运行项目生成
         filepath: './.eslintrc-auto-import.json'
       }
     }),
