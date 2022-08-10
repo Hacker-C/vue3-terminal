ca3d64637a04cf9b29de59804383f016d236622a
Hacker-C <1507559148@qq.com>
2022-08-10 10:04:30 +0800

    🔧 chore: 添加响应式语法糖

--- vite.config.ts
+++ vite.config.ts
@@ -9,7 +9,9 @@ import Components from 'unplugin-vue-components/vite'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [
-    vue(),
+    vue({
+      reactivityTransform: true
+    }),
     AutoImport({
       include: [
         /\.[tj]sx?$/, // .ts, .tsx, .js, .jsx
