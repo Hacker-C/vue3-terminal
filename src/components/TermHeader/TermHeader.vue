9e13857e04fd2f3c025c259d4288c8f83b05e1c3
Hacker-C <1507559148@qq.com>
2022-08-13 16:55:11 +0800

    💄 ui: Add buttons

--- src/components/TermHeader/TermHeader.vue
+++ src/components/TermHeader/TermHeader.vue
@@ -1,10 +1,25 @@
+<script setup lang="ts">
+import { Icon } from '@iconify/vue'
+const { currentDirName } = storeToRefs(useDirectoryStore())
+
+const close = (e: Event) => {
+  console.log('close')
+}
+</script>
+
 <template>
   <!-- 终端顶部区域 -->
   <header class="box-header">
     <div class="circle-container">
-      <span class="circle bg-red-600"></span>
-      <span class="circle bg-yellow-400"></span>
-      <span class="circle bg-green-500"></span>
+      <div @click="close" class="circle bg-red-600">
+        <Icon class="circle-icon" icon="ep:close-bold" />
+      </div>
+      <div class="circle bg-yellow-400">
+        <Icon class="circle-icon" icon="akar-icons:minus" />
+      </div>
+      <div class="circle bg-green-500">
+        <Icon class="circle-icon" icon="fa-solid:expand-arrows-alt" />
+      </div>
     </div>
     <div class="center-dir-title">
       <Icon icon="ant-design:folder-filled" class="text-[#57c5ff]" />
@@ -13,8 +28,3 @@
     <div class="circle-container"></div>
   </header>
 </template>
-
-<script setup lang="ts">
-import { Icon } from '@iconify/vue'
-const { currentDirName } = storeToRefs(useDirectoryStore())
-</script>
