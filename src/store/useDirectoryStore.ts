dc6707bbff5c9158737eeee2792ea3aa3dd45e49
Hacker-C <1507559148@qq.com>
2022-08-15 16:21:39 +0800

    💡 docs: update comments

--- src/store/useDirectoryStore.ts
+++ src/store/useDirectoryStore.ts
@@ -1,3 +1,4 @@
+// the directory system data structure
 export interface Directory {
   id: number
   name: string
@@ -6,6 +7,7 @@ export interface Directory {
   previous: Directory | null
 }
 
+// the command data structure
 export interface Command {
   commandStr: string
   isValid?: boolean
@@ -13,7 +15,7 @@ export interface Command {
   description?: string
 }
 
-// 初始化根目录
+// init the root directory
 function initDir(): Directory {
   const dir: Directory = {
     id: 0,
@@ -41,17 +43,17 @@ function initDir(): Directory {
   return dir
 }
 
-// 解析命令
+// resolve the command
 function splitCommand(command: string): string[] {
   return [command.split(' ')[0], command.split(' ').slice(1).join(' ')]
 }
 
 const useDirectoryStore = defineStore('directory', () => {
-  // 文件总目录
+  // the current directory pointer
   const dir = ref<Directory>(initDir().directories[0].directories[0])
-  // 当前目录名
+  // current directory name
   const currentDirName = computed(() => dir.value.name)
-  // 当前目录完整路径
+  // current full name of the current path
   const currentFullPath = computed(() => {
     let path = dir.value.name
     let temp = dir.value.previous
@@ -62,7 +64,7 @@ const useDirectoryStore = defineStore('directory', () => {
     return path
   })
 
-  // 合法指令（已经完成的指令）
+  // valid commands(finished commands)
   // prettier-ignore
   const ValidCommands = ['cd', 'ls', 'pwd', 'clear', 'mkdir', 'touch', 'welcome', 'help', 'echo']
   const commandDescription = [
@@ -77,22 +79,22 @@ const useDirectoryStore = defineStore('directory', () => {
     'echo [message] - echo message'
   ]
 
-  // 帮助命令信息 help
+  // help message
   const commandHelp = computed(() => {
     return ValidCommands.map((item, index) => {
       return [item, commandDescription[index]]
     })
   })
 
-  // 是否合法的命令
+  // the command is valid or not
   const isValidCommand = (commandStr: string) => {
     return ValidCommands.includes(commandStr.split(' ')[0])
   }
 
-  // 显示在屏幕上的历史命令
+  // the history command displayed on the screen
   const showCommands = ref<Command[]>([])
 
-  // 添加历史命令
+  // add a history command
   const addShowCommand = (command: Command) => {
     showCommands.value.push({
       ...command,
@@ -100,14 +102,14 @@ const useDirectoryStore = defineStore('directory', () => {
     })
   }
 
-  // 维护一个固定的上一次历史命令
+  // history path
   const historyPath = ref('/')
-  // 记录上一次的历史命令
+  // record history path
   const setHistoryPath = () => {
     historyPath.value = currentFullPath.value
   }
 
-  // 处理其他
+  // handle the unfinished commands
   const handleOther = (commandStr: string) => {
     setHistoryPath()
     addShowCommand({
