3570bd33c72845d2ee973795c02d32adbcf4e3d1
Hacker-C <1507559148@qq.com>
2022-09-25 20:37:40 +0800

    🐎 perf: img => css background

--- src/views/TermApp.vue
+++ src/views/TermApp.vue
@@ -16,25 +16,10 @@ provide(showKey, () => {
 provide(closeKey, () => {
   isOpen.value = false
 })
-
-const loaded = ref(false)
-const show = () => {
-  loaded.value = true
-}
-
-// setTimeout(() => {
-//   show()
-// }, 1000)
-onMounted(() => {
-  window.addEventListener('load', show)
-})
-onUnmounted(() => {
-  window.removeEventListener('load', show)
-})
 </script>
 
 <template>
-  <div v-show="loaded" class="screen">
+  <div class="screen bg-color">
     <div class="self-start h-screen ml-2 flex flex-col items-center">
       <div @click="openApp" class="flex flex-col items-center">
         <Icon class="w-14 h-14 bg-gray-300" icon="oi:terminal" />
@@ -52,19 +37,22 @@ onUnmounted(() => {
     </footer>
     <TermContainer v-show="isShow" v-if="isOpen" />
   </div>
-  <div v-show="!loaded" class="loading-screen">Terminal Loaidng...</div>
 </template>
 
 <style scoped>
+.bg-color {
+  background: rgb(200, 110, 161);
+  background: linear-gradient(
+    119deg,
+    rgba(200, 110, 161, 1) 3%,
+    rgba(175, 61, 200, 1) 22%,
+    rgba(77, 23, 171, 1) 100%
+  );
+}
+
 .screen {
   @apply w-screen h-screen fixed
       flex flex-col-reverse justify-center items-center
-      bg-[url('@/assets/images/bg-macos.jpg')] bg-center bg-cover
-      pt-2 font-['-apple-system','Helvetica_Neu','Helvetica','Arial','sans-serif','Microsoft_YaHei'];
-}
-
-.loading-screen {
-  @apply w-screen h-screen bg-black text-white text-xl flex items-center justify-center
-  transition-all;
+      bg-center bg-cover pt-2 font-['-apple-system','Helvetica_Neu','Helvetica','Arial','sans-serif','Microsoft_YaHei'];
 }
 </style>
