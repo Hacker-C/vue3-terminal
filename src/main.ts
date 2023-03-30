94d04fbc4644cf80aba7373dead672a6d08d3939
Hacker-C <1507559148@qq.com>
2023-03-30 11:26:43 +0800

    🚚 chore: Move styles location

--- src/main.ts
+++ src/main.ts
@@ -2,6 +2,6 @@ import { createApp } from 'vue'
 import { createPinia } from 'pinia'
 import App from './App.vue'
 
-import './assets/index.css'
+import './styles/index.css'
 
 createApp(App).use(createPinia()).mount('#app')
